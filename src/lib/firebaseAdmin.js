4ff291a55ed9cfbd18417eada108d21cc204732b
mmtkrdmr <mmtkrdmr@icloud.com>
2025-08-04 23:56:14 +0300

    Update firebaseAdmin.js

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,19 +1,17 @@
 // src/lib/firebaseAdmin.js
 import admin from 'firebase-admin';
 
-// Önce anahtar dosyamızı import ediyoruz.
-// DİKKAT: Bu yol, dosyanın projenin KÖK dizininde olduğunu varsayar.
-import serviceAccount from '../../serviceAccountKey.json'; 
-
 if (!admin.apps.length) {
   try {
+    // Ortam değişkeninden JSON verisini alıyoruz
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
     admin.initializeApp({
-      // Bu sefer JSON.parse'a falan gerek yok, direkt dosyayı veriyoruz.
       credential: admin.credential.cert(serviceAccount),
     });
-    console.log('Firebase Admin SDK dosyadan başarıyla bağlandı.');
+    console.log('Firebase Admin SDK başarıyla bağlandı.');
   } catch (error) {
-    console.error('Firebase Admin SDK bağlanırken anasının amı gibi bir hata oluştu:', error);
+    console.error('Firebase Admin SDK bağlanırken bir hata oluştu:', error);
   }
 }
 
